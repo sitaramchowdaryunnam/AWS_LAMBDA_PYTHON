c903df8f6eeb28c851b136d168d08552a8cc691e
sitaramchowdaryunnam <sitaramchowdaryunnam@gmail.com>
2021-02-25 15:52:58 +0900

    Python file created

---
+++ S3_BUCKET_LISTING.py
@@ -0,0 +1,10 @@
+import json
+import boto3
+
+def lambda_handler(event, context):
+    s3 = boto3.client('s3')
+    response = s3.list_buckets()
+    # Output the bucket names
+    print('Existing buckets:')
+    for bucket in response['Buckets']:
+        print(f'  {bucket["Name"]}')
